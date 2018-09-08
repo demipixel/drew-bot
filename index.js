7ae2d2e5d28c7ab83d0cf3dad1bf6d87368b6f9e
Lucas Simon <luke5227@gmail.com>
2018-09-07 20:58:51 -0700

    Make fun of Tony

--- index.js
+++ index.js
@@ -16,9 +16,15 @@ discord.on('message', msg => {
   
   if (['?', 'what', 'what?', 'huh', 'huh?', 'eh?'].includes(msg.content)) {
     msg.channel.send(cachedMessages[msg.channel.name].toUpperCase());
+  } else if (msg.author.id == '135627399420313600' && Math.random() <= 1/50) {
+    msg.channel.send(capitlizeFunky(msg.content));
   } else {
     cachedMessages[msg.channel.name] = msg.content;
   }
 });
+
+function capitlizeFunky(str) {
+  return str.split('').map((c,i) => i % 2 == 0 ? c.toLowerCase() : c.toUpperCase()).join('')
+}
 
 discord.login(config.discord.token);
